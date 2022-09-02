cd8dd589d41f670484f969788b8f71091e7d973b
Eugeniusz <eugenioskye@gmail.com>
2022-09-02 22:31:54 +0200

    Commit one, pairstring ready

--- src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
+++ src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
@@ -1,5 +1,104 @@
 package com.efimchick.ifmo.collections;
 
-class MedianQueue  {
+import java.util.*;
 
+class MedianQueue implements Queue<Integer> {
+
+    private PriorityQueue<Integer> lowerHalf;
+    private PriorityQueue<Integer> upperHalf;
+
+    public MedianQueue() {
+        lowerHalf = new PriorityQueue<>(Collections.reverseOrder());
+        upperHalf = new PriorityQueue<>();
+    }
+
+    @Override
+    public int size() {
+        return lowerHalf.size() + upperHalf.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return upperHalf.isEmpty() && lowerHalf.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Integer> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean offer(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public Integer remove() {
+        return null;
+    }
+
+    @Override
+    public Integer poll() {
+        return null;
+    }
+
+    @Override
+    public Integer element() {
+        return null;
+    }
+
+    @Override
+    public Integer peek() {
+        return null;
+    }
 }
--- src/main/java/com/efimchick/ifmo/collections/PairStringList.java
+++ src/main/java/com/efimchick/ifmo/collections/PairStringList.java
@@ -1,124 +1,142 @@
 package com.efimchick.ifmo.collections;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class PairStringList implements List<String> {
 
-    private List<String> elementData;
+    private ArrayList<String> list;
 
     public PairStringList() {
-        this.elementData = new ArrayList<>();
+        this.list = new ArrayList<>();
     }
 
     @Override
     public String toString() {
         return "PairStringList{" +
-                "elementData=" + elementData +
+                "elementData=" + list +
                 '}';
     }
 
-    public static void main(String[] args) {
-        List<String> test = new PairStringList();
-        test.add("zz");
-        System.out.println(test.toString());
-        System.out.println(test.size());
-    }
+
     @Override
     public int size() {
-        return elementData.size();
+        return list.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return elementData.isEmpty();
+        return list.isEmpty();
     }
 
     @Override
     public boolean contains(Object o) {
-        return elementData.contains(o);
+        return list.contains(o);
     }
 
     @Override
     public Iterator<String> iterator() {
-        return null;
+        return list.iterator();
     }
 
     @Override
     public Object[] toArray() {
-        return elementData.toArray(new String[0]);//new Object[0];
+        return list.toArray();
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        return (T[]) list.toArray();
     }
 
     @Override
     public boolean add(String s) {
-        elementData.add(s);
-        elementData.add(s);
-        return false;
+        list.add(s);
+        list.add(s);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        list.remove(o);
+        return list.remove(o);
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        return list.containsAll(c);
     }
 
     @Override
     public boolean addAll(Collection<? extends String> c) {
-        return false;
+        c.forEach(x -> {list.add(x);
+        list.add(x);
+        });
+        return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends String> c) {
-        return false;
+        AtomicInteger temp= new AtomicInteger();
+        if(index%2!=0)
+            temp.getAndIncrement();
+
+        c.forEach(s -> {
+            list.add(index+ temp.get(),s);
+            list.add(index+ temp.get(),s);
+            temp.getAndIncrement();
+            temp.getAndIncrement();
+        });
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        return list.removeAll(c);
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        return list.retainAll(c);
     }
 
     @Override
     public void clear() {
-        elementData = new ArrayList<>();
+        list = new ArrayList<>();
     }
 
     @Override
     public String get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public String set(int index, String element) {
-        return null;
-    }
+        if(index%2==0)
+            list.set(index+1,element);
+        else
+            list.set(index-1,element);
+        return list.set(index,element);    }
 
     @Override
     public void add(int index, String element) {
         if((index&1)==1) {
-            elementData.add(index + 1, element);
-            elementData.add(index + 1, element);
+            list.add(index + 1, element);
+            list.add(index + 1, element);
         } else {
-            elementData.add(index,element);
-            elementData.add(index,element);
+            list.add(index,element);
+            list.add(index,element);
         }
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        if (index%2==0){
+            list.remove(index+1);
+            return list.remove(index);
+        }
+        list.remove(index-1);
+        return list.remove(index-1);
     }
 
     @Override
